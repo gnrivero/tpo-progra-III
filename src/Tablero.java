08a8fc0585f872539e7049addbcbdc4de86c4243
grivero@navent.com <grivero@navent.com>
2018-06-20 16:03:24 -0300

    Correcciones al bug de -1 en la posicion 3 1(-1). Refactor de nombres de clases. Encapsulamiento de accesos a miembros Creo una nuevo objeto nuevaPosicion para evitar errores.

--- src/Tablero.java
+++ src/Tablero.java
@@ -1,45 +1,46 @@
-public class tablero {
-	public posicion[][] tablerito;
-	public int fila,columna;
+public class Tablero {
+		
+	public Posicion[][] tablerito;
+	public int fila, columna;
 
-	public tablero(int fila, int columna) {
-		this.tablerito = new posicion[fila][columna];
+	public Tablero(int fila, int columna) {
+		this.tablerito = new Posicion[fila][columna];
 		this.fila = fila;
 		this.columna = columna;
 	}
 	
-	public void reemplazarTablero(tablero tableroNuevo) {
+	public void reemplazarTablero(Tablero tableroNuevo) {
 		for(int i=0; i<fila; i++) {
 			for(int j=0; j<columna; j++) {
-				posicion nuevaPosicion = tableroNuevo.getPosicion(i, j);
+				Posicion nuevaPosicion = tableroNuevo.getPosicion(i, j);
 				
 				setPosicion(i, j, nuevaPosicion.getEstado(), nuevaPosicion.getPeso(), nuevaPosicion.getVisitado());
 			}
 		}
 	}
 	
-	public boolean esFin(posicion posActual, posicion posFinal) {
+	public boolean esFin(Posicion posActual, Posicion posFinal) {
 		return ((posActual.getCoordenadaX() == posFinal.getCoordenadaX()) && (posActual.getCoordenadaY() == posFinal.getCoordenadaY()));
 	}
 	
-	public boolean verificarLimites(posicion nuevaPosicion) {
+	public boolean verificarLimites(Posicion nuevaPosicion) {
 		boolean limiteMax = (nuevaPosicion.getCoordenadaX() < this.fila) && (nuevaPosicion.getCoordenadaY() < this.columna);
 		boolean limiteMin = (nuevaPosicion.getCoordenadaX() > -1) && (nuevaPosicion.getCoordenadaY() > -1);
 		return (limiteMax && limiteMin);
 	}
 	
 	/* Inicio de los Getters */
-	public posicion getPosicion(int x, int y) {
+	public Posicion getPosicion(int x, int y) {
 		return this.tablerito[x][y];
 	}
 	
-	public posicion[][] getTablero() {
+	public Posicion[][] getTablero() {
 		return this.tablerito;
 	}
 	
 	/* Inicio de los Setters */
 	public void setPosicion(int x, int y, char nuevoEstado, int nuevoPeso, boolean nuevoVisitado) {
-		tablerito[x][y]=new posicion();
+		tablerito[x][y]=new Posicion();
 		tablerito[x][y].setCoordenadas(x, y);
 		tablerito[x][y].setEstado(nuevoEstado);
 		tablerito[x][y].setPeso(nuevoPeso);
